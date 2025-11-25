d64722e2cd4466d8302be8b729a9e8c6f4dad09e
Gustavo Castilho <gustavocastilho245@gmail.com>
2025-11-24 21:23:35 -0300

    feat: header agora pode ser desativado e escondido

--- DATLAS/client/src/components/Header/Header.tsx
+++ DATLAS/client/src/components/Header/Header.tsx
@@ -1,11 +1,26 @@
 import styles from "./Header.module.css";
 import logo from "../../assets/logo-site.png";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-export function Header() {
+type HeaderProps = {
+	visible: boolean
+}
+
+export function Header({visible}: HeaderProps) {
+	const [headerVisible, setHeaderVisible] = useState<boolean>(true)
+
+	useEffect(() => {
+		if (visible){
+			setHeaderVisible(true)
+		}
+	}), [visible]
+
+	
+
 	return (
 		<>
-			<header className={styles.header}>
+			<header className={headerVisible === true ? styles.header : styles.disable}>
 				<div className={styles.logo}>
 					<img src={logo} alt="" />
 				</div>
