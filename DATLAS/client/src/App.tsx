9fd1bd24ebbe15e3ec6fde9dd77da2cf6f8c62ac
Gustavo Castilho <gustavocastilho245@gmail.com>
2025-11-25 00:24:05 -0300

    feat: rota aba quem somos adicionada

--- DATLAS/client/src/App.tsx
+++ DATLAS/client/src/App.tsx
@@ -4,6 +4,7 @@ import 'leaflet/dist/leaflet.css';
 import MapView from './components/Map/MapView.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './components/Home/Home.tsx';
+import QuemSomosComponent from './components/QuemSomos/QuemSomosComponent.tsx';
 
 function App() {
   useEffect(()=> {
@@ -18,6 +19,7 @@ function App() {
           <Routes>
             <Route path='/mapa' element={<MapView />} />
             <Route path='/' element={<Home />} />
+            <Route path='/quem-somos' element={<QuemSomosComponent />} />
           </Routes>
       </BrowserRouter>
     </>
